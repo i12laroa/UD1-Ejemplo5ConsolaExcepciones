e36a5f79c59c30aaecde807beb925065e5d15619
i12laroa <63612440+i12laroa@users.noreply.github.com>
2024-07-27 16:15:16 +0200

    Agregar archivos de proyecto.

---
+++ Ejemplo5ConsolaExcepciones/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo5ConsolaExcepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rutaArchivo;
+
+            Console.WriteLine("Escribe el nombre del fichero para leer en la ruta actual: ");
+            rutaArchivo = Console.ReadLine();
+
+            try
+            {
+                string contenido = File.ReadAllText(rutaArchivo);
+                Console.WriteLine(contenido);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Error: El archivo no se encontró.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Error: No tienes permisos para acceder al archivo.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error inesperado:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Ejemplo5ConsolaExcepciones/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Ejemplo5ConsolaExcepciones")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ejemplo5ConsolaExcepciones")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("0ad1199a-ac10-4c19-b4cb-ba39aab0eae6")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
